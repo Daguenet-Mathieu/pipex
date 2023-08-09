d7b2b872bad6b44a70074571f35e604930e45179
mathieu <mathieu.daguenet@outlook.com>
2023-08-09 08:52:26 +0200

    correction typo

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: madaguen <madaguen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 19:25:27 by madaguen          #+#    #+#             */
-/*   Updated: 2023/08/06 21:34:11 by madaguen         ###   ########.fr       */
+/*   Updated: 2023/08/09 08:51:34 by madaguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	verif_heredoc(char *s1, char *s2, int n)
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
-	if (s1[i + 1] == '\n' && s2[i - 1] == '\0')
+	if (s1[i + 1] == '\n' && s2[i + 1] == '\0')
 		return (0);
 	return (1);
 }
